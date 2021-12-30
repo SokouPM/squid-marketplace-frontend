4383cfd3298bc874a186b97fef5869b38e8308d5
Pierre Marquet <marquet_pierre@yahoo.fr>
2021-12-30 19:44:28 +0100

    minor update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,7 +13,6 @@ const Header = () => {
     <header>
       <div className={styles.headerContent}>
         <NavSideBar />
-
         <Link href="/">
           <a className={styles.logo}>
             <Image src="/logo.png" alt="logo" width={60} height={50} />

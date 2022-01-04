0b58dc9123684ec5da99929cccd406506def9240
Pierre Marquet <marquet_pierre@yahoo.fr>
2022-01-04 18:52:30 +0100

    minors update

--- src/components/body/BreadCrumbs.jsx
+++ src/components/body/BreadCrumbs.jsx
@@ -2,6 +2,7 @@ import Breadcrumbs from "nextjs-breadcrumbs";
 import styles from "/styles/components/body/Breadcrumbs.module.css";
 
 const BreadCrumbs = () => {
+  //sqdcqs
   return (
     <Breadcrumbs
       containerClassName={styles.breadcrumbs}

76d42ec2d97453ba1299bb062ac78cd3e7aaa9a0
Pierre Marquet <marquet_pierre@yahoo.fr>
2022-05-09 20:44:19 +0200

    Update back-office

--- src/components/body/admin/ArticleForm.jsx
+++ src/components/body/admin/ArticleForm.jsx
@@ -1,5 +1,8 @@
+import Image from "next/image"
+import { useState } from "react"
 import { Form, Formik, Field } from "formik"
 import * as Yup from "yup"
+import { ImCross } from "react-icons/im"
 import FormField from "../FormField"
 import categories from "../../../datas/categories.json"
 
@@ -15,31 +18,68 @@ const displayingErrorMessagesSchema = Yup.object().shape({
   price: Yup.number()
     .typeError("Le prix doit être un nombre")
     .min(0, "Le prix doit être supérieur ou égal à 0")
-    .max(99999, "Le prix doit être inférieur ou égal à 99999")
+    .max(999999, "Le prix doit être inférieur ou égal à 999999")
     .required("Le champ est requis !"),
+  category: Yup.string().required("Le champ est requis !"),
+  color: Yup.string().required("Le champ est requis !"),
   stock: Yup.number()
     .typeError("Le stock doit être un nombre")
     .min(0, "Le stock doit être supérieur ou égal à 0")
-    .max(99999, "Le stock doit être inférieur ou égal à 99999")
+    .max(999999, "Le stock doit être inférieur ou égal à 999999")
     .required("Le champ est requis !"),
 })
 
 const CategoriesList = ({ article }) => {
+  const [pictureError, setPictureError] = useState(null)
+  const [pictureList, setPicturesList] = useState([])
+
+  const checkAndAddImageOnArray = (file) => {
+    const fileExtention = file.type.split("/").pop()
+
+    if (
+      fileExtention !== "jpg" &&
+      fileExtention !== "jpeg" &&
+      fileExtention !== "png"
+    ) {
+      setPictureError("L'image doit être au format jpeg jpg ou png")
+
+      return true
+    }
+
+    if (file.size > 1048576) {
+      setPictureError("L'image est supérieure à 1Mo")
+
+      return true
+    }
+
+    setPicturesList((arr) => [...arr, file])
+
+    setPictureError(null)
+  }
+
   return (
     <Formik
       initialValues={{
         name: article ? article.name : "",
         description: article ? article.description : "",
+        category: article ? article.category : categories[0].name,
+        color: article ? article.color : "Rouge",
         price: article ? article.price : 0,
         stock: article ? article.stock : 0,
       }}
       validationSchema={displayingErrorMessagesSchema}
       onSubmit={(values) => {
-        alert(JSON.stringify(values, null, 2)) // TODO
+        if (pictureList.length < 2) {
+          setPictureError("Vous devez mettre au moins 2 images")
+
+          return
+        }
+
+        alert([JSON.stringify(values, null, 2), pictureList]) // TODO
       }}
     >
       {({ errors, touched }) => (
-        <Form className="w-5/6 p-12 border mx-auto flex flex-col items-center justify-center rounded">
+        <Form className="w-5/6 p-12 border mx-auto mb-5 flex flex-col items-center justify-center rounded">
           <FormField
             style="w-5/6 mb-5"
             label="Nom de l'article"
@@ -136,18 +176,68 @@ const CategoriesList = ({ article }) => {
               touchedType={touched.stock}
             />
           </div>
-          TODO FILE INPUT
+          <div className="w-5/6 mb-10">
+            <label htmlFor="image">
+              Ajouter une image à l'article (min. 2 / max. 6)
+            </label>
+            <input
+              type="file"
+              name="image"
+              className={`border-2 rounded py-1 px-2 w-full cursor-pointer ${
+                (pictureError && "border-red-600",
+                pictureList.length >= 6 && "opacity-25 cursor-not-allowed")
+              }`}
+              disabled={pictureList.length >= 6 ? true : false}
+              onChange={(e) => {
+                const isAnError = checkAndAddImageOnArray(
+                  e.target.files[0],
+                  e.target.value
+                )
+
+                if (!isAnError || pictureList.length >= 6) {
+                  e.target.value = null
+                }
+              }}
+            />
+            {pictureError && pictureList.length < 6 && (
+              <div className="errorField mt-1 text-red-600">{pictureError}</div>
+            )}
+          </div>
+
+          <ul className="w-5/6 grid grid-cols-3 place-content-around place-items-center gap-y-5">
+            {pictureList.map((item, index) => (
+              <li key={index} className="shadow-lg relative">
+                <Image
+                  src={URL.createObjectURL(item)}
+                  width="250"
+                  height="150"
+                  alt="product image"
+                />
+                <button
+                  className="absolute bg-red-600 text-white p-2 rounded-full -right-4 -top-4 transition-all hover:scale-110"
+                  onClick={(e) => {
+                    e.preventDefault()
+                    setPicturesList(
+                      pictureList.filter((ellement, id) => id !== index)
+                    )
+                  }}
+                >
+                  <ImCross />
+                </button>
+              </li>
+            ))}
+          </ul>
           {article ? (
             <button
               type="submit"
-              className="p-2 mr-1 w-32 rounded bg-blue-600 text-white transition-all hover:bg-blue-300"
+              className="p-2 mt-10 mr-1 w-32 rounded bg-blue-600 text-white transition-all hover:bg-blue-300"
             >
               Modifier
             </button>
           ) : (
             <button
               type="submit"
-              className="p-2 mr-1 w-32 rounded bg-green-600 text-white transition-all hover:bg-green-300"
+              className="p-2 mt-10 mr-1 w-32 rounded bg-green-600 text-white transition-all hover:bg-green-300"
             >
               Ajouter
             </button>
